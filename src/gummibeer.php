1e061d0384332f4057d411f7201d900e390f6485
Tom Witkowski <dev.gummibeer@gmail.com>
2019-11-25 15:24:08 +0100

    do not double quote

--- src/gummibeer.php
+++ src/gummibeer.php
@@ -1,5 +1,5 @@
 <?php
-//$s=array_filter(array_map('chr',range(32,126)),fn($c)=>strpos('\'"`\\',$c)===false);for($p='';strlen($p)<16;str_shuffle($p)){$p.=$s[random_int(0,count($s)-1)];}return $p;
+//$s=array_values(array_filter(array_map('chr',range(32,126)),fn($c)=>strpos('\'"`\\',$c)===false));do{for($p='';strlen($p)<16;str_shuffle($p)){$p.=$s[random_int(0,count($s)-1)];}}while(!(preg_match('/[a-z]/',$p)&&preg_match('/[A-Z]/',$p)&&preg_match('/[\d]/',$p)&&preg_match('/['.preg_quote('!#$%&()*+,-./:;<=>? @\[\]^_{|}~').']/',$p)));return $p;
 
 $s = array_values(array_filter(array_map('chr', range(32, 126)), fn ($c) => strpos('\'"`\\', $c) === false));
 
@@ -7,6 +7,6 @@ do {
     for ($p = ''; strlen($p) < 16; str_shuffle($p)) {
         $p .= $s[random_int(0, count($s) - 1)];
     }
-} while(!(preg_match('/[a-z]/', $p) && preg_match('/[A-Z]/', $p) && preg_match('/[\d]/', $p) && preg_match('/['.preg_quote('!#$%&()*+,-./:;<=>? @\[\]^_{|}~').']/', $p)));
+} while(!(preg_match('/[a-z]/', $p) && preg_match('/[A-Z]/', $p) && preg_match('/[\d]/', $p) && preg_match('/['.preg_quote('!#$%&()*+,-./:;<=>? @[]^_{|}~').']/', $p)));
 
 return $p;
