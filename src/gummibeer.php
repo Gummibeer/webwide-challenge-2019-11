d7290fb29167027bb708d3a3f7811de1f1e1bb9d
Tom Witkowski <dev.gummibeer@gmail.com>
2019-11-25 15:20:53 +0100

    fix regex

--- src/gummibeer.php
+++ src/gummibeer.php
@@ -7,6 +7,6 @@ do {
     for ($p = ''; strlen($p) < 16; str_shuffle($p)) {
         $p .= $s[random_int(0, count($s) - 1)];
     }
-} while(!(preg_match("/[a-z]/", $p) && preg_match("/[A-Z]/", $p) && preg_match("/[\d]/", $p) && preg_match("/[!#$%&()*+,\-./:;<=>? @\[\]^_{|}~]/", $p)));
+} while(!(preg_match('/[a-z]/', $p) && preg_match('/[A-Z]/', $p) && preg_match('/[\d]/', $p) && preg_match('/['.preg_quote('!#$%&()*+,-./:;<=>? @\[\]^_{|}~').']/', $p)));
 
 return $p;
