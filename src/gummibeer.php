08a81383ebf0c4ed3e3330bd708ce098e363ad17
Tom Witkowski <dev.gummibeer@gmail.com>
2019-11-25 15:02:40 +0100

    fix php solution

--- src/gummibeer.php
+++ src/gummibeer.php
@@ -1,1 +1,10 @@
-<?php $s=array_filter(array_map('chr',range(32,126)),fn($c)=>strpos('\'"`\\',$c)===false);for($p='';strlen($p)<16;str_shuffle($p)){$p.=$s[random_int(0,count($s)-1)];}return $p;
+<?php
+//$s=array_filter(array_map('chr',range(32,126)),fn($c)=>strpos('\'"`\\',$c)===false);for($p='';strlen($p)<16;str_shuffle($p)){$p.=$s[random_int(0,count($s)-1)];}return $p;
+
+$s = array_values(array_filter(array_map('chr', range(32, 126)), fn ($c) => strpos('\'"`\\', $c) === false));
+
+for ($p = ''; strlen($p) < 16; str_shuffle($p)) {
+    $p .= $s[random_int(0, count($s) - 1)];
+}
+
+return $p;
