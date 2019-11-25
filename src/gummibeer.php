e820121fbe7ab1791e240926736a5b7f15616fd0
Tom Witkowski <dev.gummibeer@gmail.com>
2019-11-25 14:56:30 +0100

    minify PHP

--- src/gummibeer.php
+++ src/gummibeer.php
@@ -1,9 +1,1 @@
-<?php
-
-$set = array_filter(array_map('chr', range(32,126)), fn($chr) => strpos('\'"`\\', $chr) === false);
-
-for ($p = ''; strlen($p) < 16; str_shuffle($p)) {
-    $p .= $set[random_int(0, count($set) - 1)];
-}
-
-return $p;
+<?php $s=array_filter(array_map('chr',range(32,126)),fn($c)=>strpos('\'"`\\',$c)===false);for($p='';strlen($p)<16;str_shuffle($p)){$p.=$s[random_int(0,count($s)-1)];}return $p;
