a49eded45e5128d9e8f136e36ac13b2ff94c2d2b
Tom Witkowski <dev.gummibeer@gmail.com>
2019-11-25 15:14:47 +0100

    enforce every charset existence

--- src/gummibeer.php
+++ src/gummibeer.php
@@ -3,8 +3,10 @@
 
 $s = array_values(array_filter(array_map('chr', range(32, 126)), fn ($c) => strpos('\'"`\\', $c) === false));
 
-for ($p = ''; strlen($p) < 16; str_shuffle($p)) {
-    $p .= $s[random_int(0, count($s) - 1)];
-}
+do {
+    for ($p = ''; strlen($p) < 16; str_shuffle($p)) {
+        $p .= $s[random_int(0, count($s) - 1)];
+    }
+} while(!(preg_match("/[a-z]/", $p) && preg_match("/[A-Z]/", $p) && preg_match("/[\d]/", $p) && preg_match("/[!#$%&()*+,\-./:;<=>? @\[\]^_{|}~]/", $p)));
 
 return $p;
